abea8d43f3f17b5b0826e58b71da4cf4972186f8
Nico Cobelo <ingcobelo@gmail.com>
2020-12-09 16:27:12 -0300

    Add cameraTapped and imagePickerController functions

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Nico Cobelo on 08/12/2020.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
+
